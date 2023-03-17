f8e3872dfdb80f1a1466ff354bba20bf5cb7d046
rrdmp15 <rdayismar@gmail.com>
2023-03-16 19:25:45 -0500

    feat: :construction: Formularios terminados y funcionales
    se arreglo la logica de la pagina para que almacenara los datos de manera adecuada dentro de los objetos a parte de que se agrego la funcionalidad de mostrar las secciones a medida que se complete cada parte

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let miFormularioSedesCampus = document.querySelector("#miFormularioSedesCampus");
 let myFormularioCampers = document.querySelector("#myFormularioCampers");
-// let myFormularioTrainers = document.querySelector("#myFormularioTrainers");
+let myFormularioTrainers = document.querySelector("#myFormularioTrainers");
 let campus = {};
 let horarios = ["6:00am - 2:00pm", "2:00pm - 10:00pm"];
 let teams = ["Team 1", "Team 2", "Team 3", "Team 4"];
@@ -10,9 +10,39 @@ let horariosIngles = ["9:00am - 10:00am", "10:00am - 11:00am", "5:00pm - 6:00pm"
 let horariosSER = ["8:00am - 9:00am", "11:00am - 12:00am", "4:00pm - 5:00pm", "6:00pm - 8:00pm"];
 let hIngles = document.getElementById("hIngles");
 let hSer = document.getElementById("hSer");
+let agregarSede = document.getElementById("agregarSede");
+let datosCampers = document.getElementById("datosCampers");
+let datosTrainers = document.getElementById("datosTrainers");
+let btnNext1 = document.getElementById("btnNext1");
+let btnNext2 = document.getElementById("btnNext2");
+let btnNext3 = document.getElementById("btnNext3");
+let botonReiniciar = document.getElementById('botonReiniciar')
 
 
+// esconder section
+datosCampers.style.display = "none";
+datosTrainers.style.display = "none";
 
+// mostrar section
+btnNext1.addEventListener("click", mostrarCampers);
+btnNext2.addEventListener("click", mostrarTrainers);
+
+function mostrarCampers(){
+    datosCampers.style.display = "block";
+    agregarSede.style.display = "none";
+}
+
+function mostrarTrainers(){
+    datosTrainers.style.display = "block";
+    datosCampers.style.display = "none";
+}
+
+// reiniciar 
+botonReiniciar.addEventListener('click', reiniciarJuego)
+
+function reiniciarJuego(){
+    location.reload();
+}
 
 // Formularios
 miFormularioSedesCampus.addEventListener("submit", (e)=>{
@@ -20,11 +50,12 @@ miFormularioSedesCampus.addEventListener("submit", (e)=>{
     let data = Object.fromEntries(new FormData(e.target))
     campus[`${data.nombreSede}`] = {Camper: [], Trainers: []};
     listaSedes();
+    actualizarOpcionesSelect();
     miFormularioSedesCampus.reset();
 })
 
 let listaSedes = ()=>{
-    let opciones = document.querySelector("[name='sede']");
+    let opciones = document.querySelector("#sede");
     opciones.innerHTML = null;
     for (let [val, id] of Object.entries(campus)) {
         opciones.insertAdjacentHTML("beforeend", `
@@ -43,6 +74,31 @@ myFormularioCampers.addEventListener("submit", (e)=>{
     console.log(campus);
     myFormularioCampers.reset();
 })
+
+myFormularioTrainers.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    let data = Object.fromEntries(new FormData(e.target));
+    console.log(data);
+    let sede = data.sede;
+    delete data.sede;
+    campus[`${sede}`]["Trainers"].unshift(data);
+    console.log(campus);
+    actualizarOpcionesSelect();
+    myFormularioTrainers.reset();
+})
+
+function actualizarOpcionesSelect() {
+    let opciones = document.querySelectorAll("[name='sede']");
+    opciones.forEach(opcion => {
+    opcion.innerHTML = null;
+    for (let [val, id] of Object.entries(campus)) {
+        opcion.insertAdjacentHTML("beforeend", `
+        <option value="${val}">${val}</option>
+        `);
+    }
+    });
+}
+
 
 // concatenar select
 function recorrer (horario, team){
@@ -134,5 +190,9 @@ selectTeam.addEventListener("change", (e)=>{
             break;
     }
 })
+
+
+
+
 // <option value=""></option>
 //  `` 
